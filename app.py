1b121a3243509e2de22e772bd99cdbbe1d25eb1b
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2025-04-18 16:21:20 +0500

    v1.2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from time import *
 from streamlit import *
 import json
+import wikipedia
+wikipedia.set_lang("en")
 
 markdown("# :rainbow[Inglizcha so'zlar ma'nosi]")
 with open('dictionary.json', 'r') as file:
@@ -12,4 +14,6 @@ tanlov = selectbox("Tanlang...",data)
 write(data[tanlov])
 tugash = time()-boshlash
 write(tugash*1_000)
-write(len(data)," ta so'z bor")
+
+write(wikipedia.search(data[tanlov]))
+text_area(len(data)," ta so'z bor")
